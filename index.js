15dfda2a864f1cf8b5afd5553216bec88f280a77
Aman Gupta <eagleax7@gmail.com>
2024-07-01 12:46:25 +0530

    major bug fixed

--- index.js
+++ index.js
@@ -74,8 +74,9 @@ dotenv.config();
 app.use(cors());
 
 app.use(express.json());
-app.use(cookieParser());
 app.use("/api/images", express.static(path.join(__dirname, "/images")));
+app.use(cookieParser());
+
 
 mongoose
   .connect(process.env.MONGO_URL, {
